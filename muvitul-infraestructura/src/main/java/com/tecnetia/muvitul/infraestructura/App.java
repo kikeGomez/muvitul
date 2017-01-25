ad54a64a67bc66be0513c10fac399e80d0ab883d
uriel <uriel@umb>
2017-01-25 16:47:33 -0600

    Commit create module muvitul

---
+++ muvitul-infraestructura/src/main/java/com/tecnetia/muvitul/infraestructura/App.java
@@ -0,0 +1,13 @@
+package com.tecnetia.muvitul.infraestructura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
