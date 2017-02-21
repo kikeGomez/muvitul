0246c68b79ad3804d9a724dcdf2480ca5d573854
uriel <uriel@umb>
2017-02-20 22:35:36 -0600

    Commit venta boletos get promociones

--- muvitul-service/src/main/java/mx/com/tecnetia/muvitul/negocio/taquilla/vo/TipoPromocionVO.java
+++ muvitul-service/src/main/java/mx/com/tecnetia/muvitul/negocio/taquilla/vo/TipoPromocionVO.java
@@ -5,4 +5,23 @@ public class TipoPromocionVO {
 	private String nombre;
 	private String descripcion;
 	
+	public Integer getIdTipoPromocion() {
+		return idTipoPromocion;
+	}
+	public void setIdTipoPromocion(Integer idTipoPromocion) {
+		this.idTipoPromocion = idTipoPromocion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 }
