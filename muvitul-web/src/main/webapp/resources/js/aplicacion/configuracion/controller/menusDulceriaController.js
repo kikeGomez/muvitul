2be2ece7166eb0a28bdc9bf2c242358b95cb0e32
fjmb <fjmb.mx@gmail.com>
2017-04-09 22:02:13 -0500

    CombosDulceria

--- muvitul-web/src/main/webapp/resources/js/aplicacion/configuracion/controller/menusDulceriaController.js
+++ muvitul-web/src/main/webapp/resources/js/aplicacion/configuracion/controller/menusDulceriaController.js
@@ -14,10 +14,7 @@ var MenusDulceriaController = angular.module('indexModule').controller("MenusDul
 		console.log(array);
 		console.log(objeto);
 		console.log(index);
-
 		 array.splice(index, 1);
- 
-
 	}
 	$scope.onChange = function (e, file) {
 		var data = file[0];
@@ -25,39 +22,12 @@ var MenusDulceriaController = angular.module('indexModule').controller("MenusDul
 	  };
 	//Consultar productos 
 	$scope.consultarProductos =function( ){
-		
-		
 		menusDulceriaService.consultarProductos().success(function(data) {	
 				console.log(data);
 				$scope.listaProductos=data;
  		  }).error(function(data) {
  				console.log(data);
-
 		  });
-		
-//		$scope.listaProductos =[
-//		   		             {
-//		   		            	    "index": 0,
-//		   		            	    "nombre": "Nachos"
-//		   		            	  },
-//		   		            	  {
-//		   		            	    "index": 1,
-//		   		            	    "nombre": "Chocolates M&M"
-//		   		            	  },
-//		   		            	  {
-//		   		            	    "index": 2,
-//		   		            	    "nombre": "Refresco CH"
-//		   		            	  },
-//		   		            	  {
-//		   		            	    "index": 3,
-//		   		            	    "nombre": "Refresco M "
-//		   		            	  },
-//		   		            	  {
-//		   		            	    "index": 4,
-//		   		            	    "nombre": " Palomitas M"
-//		   		            	  }
-//		   		            	];
-	
 	}
 	
 	$scope.consultarPaquetes =function( ){
@@ -66,48 +36,7 @@ var MenusDulceriaController = angular.module('indexModule').controller("MenusDul
 			$scope.listaPaquetes=data;
 		  }).error(function(data) {
 				console.log(data);
-
 	  });
-//		$scope.listaPaquetes=[
-//		                 {
-//		            	    "index": 0,
-//		            	    "productos": "2 Refresco CH, 1 Palomitas M",
-//		            	    "nombre": "Combo  Nachos2",
-//		            	    "precio": "$ 109.00"
-//		            	  },
-//		            	  {
-//		            	    "index": 1,
-//		            	    "productos": "2 Refresco M, 1 Nachos ",
-//		            	    "nombre": "Combo Pareja 2",
-//		            	    "precio": "$ 100.00"
-//		            	  },
-//		            	  {
-//		            	    "index": 2,
-//		            	    "productos": "2 Refresco CH, 1 Palomitas M",
-//		            	    "nombre": "Combo Nachos",
-//		            	    "precio": "$ 100.00"
-//		            	  },
-//		            	  {
-//		            	    "index": 3,
-//		            	    "productos": "2 Refresco CH, 1 Palomitas M",
-//		            	    "nombre": "Combo  Nachos2",
-//		            	    "precio": "$ 110.00"
-//		            	  },
-//		            	  {
-//		            	    "index": 4,
-//		            	    "productos": "2 Refresco M, 1 Nachos ",
-//		            	    "nombre": "Combo Pareja 2",
-//		            	    "precio": "$ 110.00"
-//		            	  }
-//		            	];
-		
-//		console.log($scope.listaPaquetes)
-//		menusDulceriaService.consultarPaquetes().success(function(data) {	
-//				console.log(data);
-// 		  }).error(function(data) {
-// 				console.log(data);
-//
-//		  });
 	}
 
 	 $scope.guardarPaqueteConfirm = function(paquete){
